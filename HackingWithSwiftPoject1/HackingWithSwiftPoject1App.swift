424eb4c2a9f3055496826fea4545a0ccafc70502
daniel79438 <daniel79438@gmail.com>
2025-05-15 08:45:43 +0100

    Initial Commit

---
+++ HackingWithSwiftPoject1/HackingWithSwiftPoject1App.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftPoject1App.swift
+//  HackingWithSwiftPoject1
+//
+//  Created by Daniel Harris on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftPoject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
